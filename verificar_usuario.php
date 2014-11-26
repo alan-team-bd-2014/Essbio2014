a07d8ed13476ce62ff3d1b60465fdd23b9d3ae03
jcardenaslie <jcardenas.lie@gmail.com>
2014-11-26 05:01:48 -0300

    verificar_usuario en index.html

--- verificar_usuario.php
+++ verificar_usuario.php
@@ -1,12 +1,6 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <?php
 
-function verificarUsuario(){
-    if(isset($usuario)){
-        
-    }
-}
-
 
 $usuario = $_POST["usuario"];
 $contra =$_POST["pass"];
@@ -21,20 +15,20 @@ if(is_numeric ($contra )){
 			$result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
 	$num= pg_num_rows($result);
 	
-        $adminCode = "SELECT * FROM Essbio.ciudadano WHERE nombre='$usuario' AND clave='$contra'";
+        //$adminCode = "SELECT c.admin_code FROM Essbio.ciudadano as c WHERE nombre='$usuario' AND clave='$contra'";
 	
-        if($adminCode!= null){
-          header('Location: welcome_admin.php');  
-        }else{
+        
+	if($num >0){
+            echo "Bienvenido $usuario";
             header('Location: welcome_usuario.php');
         }
-	if($num >0){
-	echo "Bienvenido $usuario";}
-	else 
+	else {
 		echo "usuario y/o contraseña incorrecta";
-	
+        }
 }else{ 
     echo "La contraseña sólo puede contener números";
-    header('Location: index.php');
+    header('Location: index.html');
+    
+    
 }
 ?>
