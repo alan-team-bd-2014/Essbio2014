bc859a375846ea29893ea3127a7ad33f0ff2abab
Alan112358 <alan10.yo@gmail.com>
2014-11-26 05:31:30 -0300

    listoco

--- verificar_usuario.php
+++ verificar_usuario.php
@@ -3,32 +3,25 @@
 
 
 $usuario = $_POST["usuario"];
-$contra =$_POST["pass"];
+$contra =$_POST["pwd"];
 
 if(is_numeric ($contra )){
 
-	$dbconn = pg_connect("host=plop.inf.udec.cl port=5432 dbname=BDI-g user=BDI-7 password=bdi7")
-        or die('No se ha podido conectar: ' );
+$dbconn = pg_connect("host=plop.inf.udec.cl port=5432 dbname=BDI-g user=BDI-7 password=bdi7")
+    or die('No se ha podido conectar: ' );
 	
 
 	$query = "SELECT * FROM Essbio.ciudadano WHERE nombre='$usuario' AND clave='$contra'";
 			$result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
 	$num= pg_num_rows($result);
+			
 	
-        //$adminCode = "SELECT c.admin_code FROM Essbio.ciudadano as c WHERE nombre='$usuario' AND clave='$contra'";
-	
-        
 	if($num >0){
-            echo "Bienvenido $usuario";
-            header('Location: welcome_usuario.php');
-        }
-	else {
+	echo "Bienvenido $usuario";}
+	else 
 		echo "usuario y/o contraseña incorrecta";
-        }
-}else{ 
-    echo "La contraseña sólo puede contener números";
-    header('Location: index.html');
-    
-    
-}
+	
+	}
+	else echo "La contraseña sólo puede contener números";
 ?>
+
